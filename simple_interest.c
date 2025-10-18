9223ccbe1b1c894ecd92b568fabc54a7f4e68761
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-18 04:54:39 +0100

    feat: simple_interest
    add functionality to take user input

--- simple_interest.c
+++ simple_interest.c
@@ -6,14 +6,13 @@ int main()
     int p, n;
     float r, si;
 
-    p = 1000;
-    n = 3;
-    r = 8.5;
+    printf("Enter values of p, n, r \n");
+    scanf("%d %d %f", &p, &n, &r);
     
     /* formula for simple interest */
-    si = p * n * r /100;
+    si = p * n * r / 100;
     
-    printf("%f\n", si);
+    printf("%f \n", si);
     
     return 0;
 }
