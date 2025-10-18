0cdfa85bae3c940133738f751867638aea9ac98f
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-18 07:13:01 +0100

    feat: marks
    initial commit

---
+++ marks.c
@@ -0,0 +1,23 @@
+/* Aggregate and percentage marks calculation */
+
+# include <stdio.h>
+
+int main()
+{
+    float max_marks, agg, perc;
+    float sub1, sub2, sub3, sub4, sub5;
+
+    max_marks = 100;
+
+    printf("Enter marks of 5 subjects:\n");
+    scanf("%f %f %f %f %f", &sub1, &sub2, &sub3, &sub4, &sub5);
+
+    /* aggregate and percentage marks */
+    agg = sub1 + sub2 + sub3 + sub4 + sub5;
+    perc = agg / (max_marks * 5) * 100;
+
+    printf("Aggregate marks: %f\n", agg);
+    printf("Percentage marks: %f\n", perc);
+    
+    return 0;
+}
