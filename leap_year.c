c515598379b9a8e823e9b685d46f92939a137ace
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-19 12:21:16 +0100

    feat: leap_year
    initial commit.
    problem 3.3

---
+++ leap_year.c
@@ -0,0 +1,28 @@
+/* Identify if year is leap year */
+
+# include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter year: ");
+    scanf("%d", &year);
+
+    if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+                printf("%d is a leap year\n", year);
+            else
+                printf("%d is not leap year\n", year);
+        }
+    else
+        {
+            if (year % 4 == 0)
+                printf("%d is a leap year\n", year);
+            else
+                printf("%d is not leap year\n", year);
+        }
+    
+    return 0;
+}
