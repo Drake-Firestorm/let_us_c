4960d704a23746642c00a7a4accb49d623c1c778
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-21 14:01:33 +0100

    feat: youngest
    use conditional operators
    Exercises 4.d.c

--- youngest.c
+++ youngest.c
@@ -10,28 +10,20 @@ int main()
     scanf("%d %d %d", &a, &b, &c);
 
     // compare the three ages to identify the youngest
-    if (a < b)
-        {
-            if (a < c)
-                printf("%d is the youngest\n", a);
-            else
-                {
-                    if (a > c)
-                        printf("%d is the youngest\n", c);
-                    else
-                        printf("%d and %d is the youngest and of same age\n", a, c);
-                }
-        }
-    else
-        {
-            if (b < c)
-                printf("%d is the youngest\n", b);
-            else
-                {
-                    if (b > c)
-                        printf("%d is the youngest\n", c);
-                    else
-                        printf("%d and %d is the youngest and of same age\n", b, c);
-                }
-        }
+    if ((a < b) && (a < c))
+        printf("%d is the youngest\n", a);
+    else if ((a < b) && (a > c))
+        printf("%d is the youngest\n", c);
+    else if ((b < a) && (b < c))
+        printf("%d is the youngest\n", b);
+    else if ((a < b) && (a == c))
+        printf("%d and %d is the youngest and of same age\n", a, c);
+    else if ((b < a) && (b == c))
+        printf("%d and %d is the youngest and of same age\n", b, c);
+    else if ((a < c) && (a == b))
+        printf("%d and %d is the youngest and of same age\n", a, b);
+    else if ((a == b) && (a == c))
+        printf("All of same age\n");
+
+    return 0;
 }
