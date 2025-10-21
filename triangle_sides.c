f4bde2fb7932be711407d5599506a763fc4cba9e
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-21 09:55:28 +0100

    feat: triangle_sides
    add check for type of triangle
    Exercises 4.d.a

--- triangle_sides.c
+++ triangle_sides.c
@@ -1,6 +1,7 @@
 /* Validate triangle 2 - using length of sides */
 
 # include <stdio.h>
+# include <math.h>
 
 int main()
 {
@@ -9,12 +10,38 @@ int main()
     printf("Enter the lengths of 3 sides of triangle: ");
     scanf("%d %d %d", &s1, &s2, &s3);
 
-    if (s1 > s2 && s1 > s3 && s1 < (s2 + s3))
+    if (s1 >= s2 && s1 >= s3 && s1 < (s2 + s3))
+    {
         printf("Triangle is valid\n");
-    else if (s2 > s1 && s2 > s3 && s2 < (s1 + s3))
+        if (s1 == s2 && s1 == s3)
+            printf("Triangle is equilateral\n");
+        else if (s2 == s3)
+            printf("Triangle is isosceles\n");
+        else if (pow(s1, 2) == (pow(s2, 2) + pow(s3, 2)))
+            printf("Triangle is right angled\n");
+        else
+            printf("Triangle is scalene\n");
+    }
+    else if (s2 >= s1 && s2 >= s3 && s2 < (s1 + s3))
+    {
         printf("Triangle is valid\n");
-    else if (s3 > s1 && s3 > s2 && s3 < (s1 + s2))
+        if (s1 == s3)
+            printf("Triangle is isosceles\n");
+        else if (pow(s2, 2) == (pow(s1, 2) + pow(s3, 2)))
+            printf("Triangle is right angled\n");
+        else
+            printf("Triangle is scalene\n");
+    }
+    else if (s3 >= s1 && s3 >= s2 && s3 < (s1 + s2))
+    {
         printf("Triangle is valid\n");
+        if (s1 == s2)
+            printf("Triangle is isosceles\n");
+        else if (pow(s3, 2) == (pow(s1, 2) + pow(s2, 2)))
+            printf("Triangle is right angled\n");
+        else
+            printf("Triangle is scalene\n");
+    }
     else
         printf("Triangle is not valid\n");
 
