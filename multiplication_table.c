0b5099623a97fb66c70282af6bc7ed9c472e6833
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-22 14:26:51 +0100

    feat: multiplication_table
    initial commit
    Problem 6.b.a

---
+++ multiplication_table.c
@@ -0,0 +1,16 @@
+/* Multiplication table */
+
+# include <stdio.h>
+
+int main()
+{
+    int i, num;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", num, i, num * i);
+
+    return 0;
+}
