56f63ee8d23b72c5694cd2e30e4d436b45797b38
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-26 11:59:27 +0000

    perf: factorial
    move code in main to function

--- factorial.c
+++ factorial.c
@@ -2,6 +2,8 @@
 
 # include <stdio.h>
 
+int factorial(int );
+
 int main()
 {
     int i, num, fact;
@@ -9,12 +11,19 @@ int main()
     printf("Enter number: ");
     scanf("%d", &num);
 
-    fact = i = 1;
-    while(i <= num)
-    {
-        fact *= i;
-        i += 1;
-    }
+    fact = factorial(num);
 
     printf("Factorial of %d is %d\n", num, fact);
+}
+
+
+int factorial(int num)
+{
+    int i, fact;
+
+    fact = 1;
+    for (i = 1; i <= num; i++)
+        fact *= i;
+
+    return (fact);
 }
