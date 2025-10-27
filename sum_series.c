1c393825b05b22186e7755d2c4d21ca5c6bdf5e2
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-27 17:51:06 +0000

    perf: sum_series
    using recursion
    Exercise 10.b.b

--- sum_series.c
+++ sum_series.c
@@ -1,23 +1,33 @@
-/* Sum of first seven terms of a series */
+/* Sum of first N terms of a series */
 
 # include <stdio.h>
+
+int sum_n(int);
 
 int main()
 {
     int n;
-    float sum, fact;
+    float sum;
 
-    n = fact = 1;
-    sum = 0;
-    while (n <= 7)
-    {
-        fact *= n;
-        sum += n / fact;
+    printf("Enter max terms: ");
+    scanf("%d", &n);
 
-        n++;
-    }
+    sum = sum_n(n);
 
     printf("Sum of series %f\n", sum);
 
     return 0;
+}
+
+
+int sum_n(int number)
+{
+    int s;
+
+    if(number == 1)
+        return 1;
+    else
+        s = number + sum_n(number - 1);
+
+    return s;
 }
