6786b200b07dd1a40a68314120386d2a4055cf21
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-28 09:06:09 +0000

    perf: simple_interest
    move logic to header file
    Exercises 12.c.c

---
+++ headers/interest.h
@@ -0,0 +1,4 @@
+/* Macro definitions of Simple Interest and Amount */
+
+#define SI(p, r, t) (p * r * t / 100)
+#define AMOUNT(p, si) (p + si)
--- simple_interest.c
+++ simple_interest.c
@@ -1,18 +1,20 @@
-/* Calculation of simple interest */
-/* Author: gekay Date: 25/09/2022 */
-# include <stdio.h>
+/* Simple Interest and Amount */
+
+#include <stdio.h>
+#include "./headers/interest.h"
+
 int main()
 {
-    int p, n;
-    float r, si;
+    float p, r, t, si, amt;
 
-    printf("Enter values of p, n, r \n");
-    scanf("%d %d %f", &p, &n, &r);
-    
-    /* formula for simple interest */
-    si = p * n * r / 100;
-    
-    printf("%f \n", si);
-    
+    printf("Enter principal, interest rate (in %%) and period: ");
+    scanf("%f %f %f", &p, &r, &t);
+
+    si = SI(p, r, t);
+    amt = AMOUNT(p, si);
+
+    printf("Simple Interest is %f\n", si);
+    printf("Amount is %f\n", amt);
+
     return 0;
 }
