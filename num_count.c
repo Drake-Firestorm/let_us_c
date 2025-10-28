5679570b4648f022da5bbae3e60476a4767b43ef
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-10-28 11:17:49 +0000

    perf: num_count
    use arrays
    Exercise 13.b.a

--- num_count.c
+++ num_count.c
@@ -4,37 +4,42 @@
 
 int main()
 {
-    int num, pos, neg, zero, min, max;
-    char ans;
+    int i, num[25], pos, neg, zero, min, max, even, odd;
+    
+    printf("Enter 25 numbers in array:");
+    for(i = 0; i < 25; i++)
+        scanf("%d", &num[i]);
 
-    pos = neg = zero = min = max = 0;
-    ans = 'y';
-    while (ans == 'y')
+    pos = neg = zero = even = odd = 0;
+    min = max = num[0];
+    
+    for(i = 0; i < 25; i++)
     {
-        printf("Enter number: ");
-        scanf("%d", &num);
-
-        if (num > 0)
+        if (num[i] > 0)
             pos ++;
-        else if (num < 0)
+        else if (num[i] < 0)
             neg ++;
-        else if (num == 0)
+        else if (num[i] == 0)
             zero ++;
         
-        if (num > max)
-            max = num;
+        if (num[i] > max)
+            max = num[i];
 
-        if (num < min)
-            min = num;
+        if (num[i] < min)
+            min = num[i];
 
-        printf("Continue (y/n): ");
-        scanf(" %c", &ans);
+        if(num[i] % 2 == 0)
+            even++;
+        else
+            odd++;
     }
 
     printf("Positive count: %d\n", pos);
     printf("Negative count: %d\n", neg);
     printf("Zero count: %d\n", zero);
     printf("Range of %d and %d: %d\n", min, max, max - min);
+    printf("Even count: %d\n", even);
+    printf("Odd count: %d\n", odd);
 
     return 0;
 }
