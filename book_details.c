8a70e041730175fa53ced0606b7c2a6fb43a8666
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-12-06 17:18:38 +0000

    perf: book_details
    using struct
    17.1

--- book_details.c
+++ book_details.c
@@ -5,20 +5,27 @@ void clear_input_buffer();
 
 int main()
 {
-    char name[3];
-    float price[3];
-    int pages[3], i;
+    struct book
+    {
+        char name;
+        float price;
+        int pages;
+    };
+
+    struct book b1, b2, b3;
 
     printf("Enter names, prices and no. of pages of 3 books\n");
-    for(i = 0; i < 3; i++)
-    {
-        scanf("%c %f %d", &name[i], &price[i], &pages[i]);
+    scanf("%c %f %d", &b1.name, &b1.price, &b1.pages);
         clear_input_buffer();
-    }
-
+    scanf("%c %f %d", &b2.name, &b2.price, &b2.pages);
+        clear_input_buffer();
+    scanf("%c %f %d", &b3.name, &b3.price, &b3.pages);
+        clear_input_buffer();
+    
     printf("And this is what you entered\n");
-    for(i = 0; i < 3; i++)
-        printf("%c %f %d\n", name[i], price[i], pages[i]);
+    printf("%c %f %d\n", b1.name, b1.price, b1.pages);
+    printf("%c %f %d\n", b2.name, b2.price, b2.pages);
+    printf("%c %f %d\n", b3.name, b3.price, b3.pages);
 
     return 0;
 }
