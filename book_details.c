c7379636b3a12f0e83d63c3c1b667924b4488794
Drake-Firestorm <drakefirestorm@hotmail.com>
2025-12-07 13:46:41 +0000

    feat: book_details
    passing struct elements

--- book_details.c
+++ book_details.c
@@ -1,36 +1,40 @@
-/* Book details */
+/* Passing individual structure elements */
 #include <stdio.h>
 
-void clear_input_buffer();
+struct book     // declare globally to use in functions
+{
+    char name[25], author[25];
+    int pages;
+};
+
+void display1(char *, char *, int);
+void display2(struct book);
+void display3(struct book *);
 
 int main()
 {
-    struct book
-    {
-        char name;
-        float price;
-        int pages;
-    };
-
-    struct book b[10];
-    int i, dh;
-
-    for(i = 0; i < 10; i++)
-    {
-        printf("Enter names, prices and no. of pages\n");
-        scanf("%c %f %d", &b[i].name, &b[i].price, &b[i].pages);
-        clear_input_buffer();
-    }
-
-    printf("And this is what you entered\n");
-    for(i = 0; i < 10; i++)
-        printf("%c %f %d\n", b[i].name, b[i].price, b[i].pages);
+    struct book b1 = {"Let Us C", "YPK", 464};
+    
+    display1(b1.name, b1.author, b1.pages); // mixed call - reference & value
+    display2(b1);   // struct variable
+    display3(&b1);  // struct pointer
 
     return 0;
 }
 
-void clear_input_buffer(){
-    int c;
-    while((c = getchar()) != '\n' && c != EOF);
+void display1(char *n, char *a, int pg)
+{
+    printf("%s %s %d\n", n, a, pg);
 }
 
+
+void display2(struct book b)
+{
+    printf("%s %s %d\n", b.name, b.author, b.pages);
+}
+
+
+void display3(struct book *pb)
+{
+    printf("%s %s %d\n", pb->name, pb->author, pb->pages);  // struct pointer -> struct elements
+}
